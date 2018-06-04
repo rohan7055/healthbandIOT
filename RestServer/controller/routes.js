6eae6f061fa95a4481387393c5566a4f392d512b
rohan thakur <ron2prince@gmail.com>
2018-06-04 22:58:51 +0530

    Rohan commit

--- RestServer/controller/routes.js
+++ RestServer/controller/routes.js
@@ -1,6 +1,8 @@
 var exports = module.exports = {};
 var mysql=require('../db/utils');
 const md5=require('md5');
+const jwt=require('jsonwebtoken');
+var secretKey="myrhband.tech"
 
 exports.register=function(req,res,next)
 {
@@ -67,8 +69,101 @@ exports.login=function(req,res,next){
   if(type=="patient")
   {
 
+    mysql.getPatient(req.body.username,function(err,result)
+    {
+      if(err)
+      {
+        console.log(err)
+        res.status(401).json({
+          status:false,
+          message:"Auth Failed Try Again"
+        });
+      }
+      else {
+
+        if(result[0].password==md5(req.body.password))
+        {
+          const token=jwt.sign(
+            {
+              phone:result[0].phone,
+              username:result[0].username
+            },
+            secretKey,
+            {
+              expiresIn:"3h"
+            });
+           //token aquired send jwt token
+            res.status(200).json(
+              {
+                status:true,
+                message:"Successfully Logged In",
+                data:result[0],
+                session:token
+              }
+            )
+        }
+        else {
+          //email and password doesnt match
+          res.status(401).json({
+            status:false,
+            message:"Auth Failed Try Again"
+          });
+
+        }
+
+
+
+      }
+    });
+
   }else if(type="doctor")
   {
+    mysql.getDoctor(req.body.username,function(err,result)
+    {
+      if(err)
+      {
+        console.log(err)
+        res.status(401).json({
+          status:false,
+          message:"Auth Failed Try Again"
+        });
+      }
+      else {
+
+        if(result[0].password==md5(req.body.password))
+        {
+          const token=jwt.sign(
+            {
+              phone:result[0].phone,
+              username:result[0].username
+            },
+            secretKey,
+            {
+              expiresIn:"3h"
+            });
+           //token aquired send jwt token
+            res.status(200).json(
+              {
+                status:true,
+                message:"Successfully Logged In",
+                data:result[0],
+                session:token
+              }
+            )
+        }
+        else {
+          //email and password doesnt match
+          res.status(401).json({
+            status:false,
+            message:"Auth Failed Try Again"
+          });
+
+        }
+
+
+
+      }
+    });
 
   }else {
     res.status(401).json({
